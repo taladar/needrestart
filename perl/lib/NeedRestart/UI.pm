fcb3a50c4f717e5dbaf9206cb1d13bfa16ce4f8b
Maximilian Philipps <mphilipps@saltation.de>
2019-02-27 13:09:25 +0100

    Revert "check whether we can open the tty before trying to get it's size"
    This reverts commit eaafed314586c601320f9c966797419871cce7ac.

--- perl/lib/NeedRestart/UI.pm
+++ perl/lib/NeedRestart/UI.pm
@@ -39,8 +39,6 @@ sub new {
     }, $class;
 }
 
-sub checktty() { no autodie; return open(my $tty, '+<', '/dev/tty'); }
-
 sub wprint {
     my $self = shift;
     my $fh = shift;
@@ -51,9 +49,7 @@ sub wprint {
     # only wrap output if it is a terminal
     if (-t $fh) {
 	# workaround Debian Bug#824564 in Term::ReadKey: pass filehandle twice
-        # -  don't even try
-        # if we can't open a tty
-	my ($cols) = checktty()?GetTerminalSize($fh, $fh):(50);
+	my ($cols) = GetTerminalSize($fh, $fh);
 	$columns = $cols if($cols);
 
 	print $fh wrap($sp1, $sp2, $message);
